40c3130080487482b589c4fed250149915a23f2c
seb napo <naposebastien@gmail.com>
2019-12-18 20:34:05 +0100

    Ajout des modifications pour la création du JSON

--- API/api.py
+++ API/api.py
@@ -9,13 +9,16 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/make/model')
 def hello_world():
 	list_parser = lambda x: x[1:-1].split(',')
 	catalogue = pd.read_csv("../datasets/catalogue.tsv",sep="\t",converters={'keywords': list_parser,"concepts": list_parser})
 	catalogue.columns = ['id', 'title', 'language', 'type', 'keywords', 'concepts']
 	catalogue.set_index("id", inplace=True)
-	mymodel = Word2Vec.load("../model/word2vec300SG")
+	test = catalogue["keywords"].to_numpy()
+	mymodel = gensim.models.Word2Vec(test,size=1000,min_count=1,batch_words=4000,window=20)
+	mymodel.save("./model/word2vec.model")
+	
 	return 'Hello, World!'
 
 @app.route('/getKNN/<int:voisins>/<string:concept>')
@@ -24,3 +27,32 @@ def getKNN(voisins,concept):
 	result = mymodel.wv.similar_by_word(concept)[:voisins]
 	print(result)
 	return jsonify(result)
+
+@app.route('/get/oerInfo/<int:id>')
+def getOERInfos(id):
+	PLATFORM_URL = "https://platform.x5gon.org/api/v1"
+	get_specific_materials_endpoint = "/oer_materials/{}"
+	response = requests.get(PLATFORM_URL + get_specific_materials_endpoint.format(id))
+	r_json = response.json()
+	print(r_json)
+	return jsonify(r_json)
+
+@app.route('/get/oerContent/<int:id>')
+def getOERContent(id):
+	PLATFORM_URL = "https://platform.x5gon.org/api/v1"
+	get_specific_materials_endpoint = "/oer_materials/{}/contents/"
+	response = requests.get(PLATFORM_URL + get_specific_materials_endpoint.format(id))
+	r_json = response.json()
+	return jsonify(r_json)
+
+@app.route('/get/recommandation/<string:concept>')
+def getRecommandation(concept):
+	PLATFORM_URL = "http://wp3.x5gon.org/searchengine/v1"
+	HEADERS = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json',
+	}
+	data = {"text": concept, "model_type": "doc2vec", "remove_duplicates": 1, "nb_wikiconcepts": 5, "return_wikisupport": 0 }
+	r = requests.post(url = PLATFORM_URL, data =json.dumps(data),headers=HEADERS)
+	r_json = r.json()
+	return jsonify(r_json)
--- Website/assets/js/myjs.js
+++ Website/assets/js/myjs.js
@@ -1,18 +1,50 @@
 var myLink = document.getElementById('search');
 
+
 myLink.onclick = function(){
     var concept = document.getElementById('concept');
     // Create a request variable and assign a new XMLHttpRequest object to it.
     var request = new XMLHttpRequest()
+    var conceptVal = concept.value
     // Open a new connection, using the GET request on the URL endpoint
     request.open('GET', 'http://127.0.0.1:5000/getKNN/4/'+concept.value, true)
+    var list = new Array()
+    var index=0;
     request.onload = function() {
         var data = JSON.parse(this.response)
-        data.forEach(neigh => {
-            // Log each movie's title
-            console.log(neigh)
-        })
+        var listConcept = [];
+        var listDocs = [];
+        var listNames = [];
+
+        for(x = 0; x < 4 ; x++){
+            listConcept[data[x][0]]= []
+            currentName = data[x][0]
+            listNames[x] = currentName;
+            var request2 = new XMLHttpRequest()
+            console.log(currentName)
+            request2.open('GET', 'http://127.0.0.1:5000/get/recommandation/'+currentName, true)
+            var index = 0;
+            request2.onload = function() {
+                var data = JSON.parse(this.response)
+                for(i = 0; i < 4 ; i ++) {
+                    concept = {}
+                    rec = data["output"]["rec_materials"]
+                    concept = {"name" : rec[i]["title"],
+                        "description": rec[i]['description'],
+                        "url" : rec[i]['url'] };
+                    console.log(currentName)
+                    listDocs[i] = concept
+                }
+                listConcept[listNames[index]] = listDocs;
+                listDocs = []
+                index++;
+            }
+            request2.send()
+        }
+        var myJsonString = JSON.stringify(listConcept);
+        console.log(myJsonString)
+        console.log(listConcept)
     }
     // Send request
     request.send()
-}
+};
